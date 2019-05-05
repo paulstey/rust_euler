9861dcbbc971d24666985df7cc3377ba8022b885
paulstey <paulstey@gmail.com>
2019-05-05 11:16:10 +0900

    finally have primes module working from lib.rs

--- prob3/src/main.rs
+++ prob3/src/main.rs
@@ -1,45 +1,8 @@
 
-// Seive basics: 
-//   1. Cross out multiples of 2, then multiples of 3, etc...
-//   2. By "cross out" we probably mean flip a bit in `mask`
+extern crate prob3;
 
-fn primes(n: u64) -> Vec<u64> {
-    let nums: Vec<u64> = (2..=(n+3)).collect();
-    let mut not_prime = vec![false; (n+1) as usize];
-    // let prime_flag = vec![false; n as usize];
-
-    for i in 0..=n as usize {
-        println!("{}", i);
-        // println!("length not_prime: {}", not_prime.len());
-        
-        if not_prime[i] {
-            continue;
-        }
-
-        for j in (i+1)..=n as usize {
-            let remain = nums[j] % &nums[i];
-
-            println!("{} % {}: {}", nums[j], nums[i], remain);
-
-            if remain == 0 {
-                not_prime[j] = true;
-                println!("{} is not prime: {}", nums[j], not_prime[j]);
-            }   
-        }
-    }
-    let mut prime_nums: Vec<u64> = Vec::new();
-    for i in 0..=n as usize {
-        if not_prime[i] {
-            continue;
-        }
-        else {
-            prime_nums.push(nums[i])
-        }
-    }
-    
-    prime_nums
-}
+use self::prob3::primes;
 
 fn main() {
-    println!("{:?}", primes(20));
+    println!("{:?}", primes::primes(1300));
 }
---
+++ prob3/src/primes.rs
@@ -0,0 +1,45 @@
+// primes.rs
+
+// Seive basics: 
+//   1. Cross out multiples of 2, then multiples of 3, etc...
+//   2. By "cross out" we probably mean flip a bit in `mask`
+
+pub fn primes(n: u64) -> Vec<u64> {
+    let nums: Vec<u64> = (2..=(n+3)).collect();
+    let mut not_prime = vec![false; (n+1) as usize];
+    
+    for i in 0..=n as usize {
+
+        // Skip the rest of the main loop if we've already crossed off this number        
+        if not_prime[i] {
+            continue;
+        }
+        
+        // If we haven't crossed off this number, then we walk through all the numbers 
+        // and cross out the multiples of `numb[i]` appearing in `nums`.
+        for j in (i+1)..=n as usize {
+            if not_prime[j] {
+                continue;
+            }
+            let remain = nums[j] % nums[i];
+            
+            // println!("{} % {}: {}", nums[j], nums[i], remain);
+            
+            if remain == 0 {
+                not_prime[j] = true;
+                // println!("{} is not prime: {}", nums[j], not_prime[j]);
+            }   
+        }
+    }
+    let mut prime_nums: Vec<u64> = Vec::new();
+    for i in 0..=n as usize {
+        if not_prime[i] {
+            continue;
+        }
+        else {
+            prime_nums.push(nums[i])
+        }
+    }
+    
+    prime_nums
+}
