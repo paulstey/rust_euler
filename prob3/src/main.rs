7c32e52a9bb289f4f22b76fcc0bccc607c8a3134
paulstey <paulstey@gmail.com>
2019-12-13 11:59:19 -0500

    added timing

--- prob3/src/main.rs
+++ prob3/src/main.rs
@@ -1,8 +1,15 @@
 
+extern crate time;
 // extern crate prob3;
 
 use prob3::primes;
 
+
+
 fn main() {
-    println!("{:?}", primes::primes(13000));
+    let t0: u64 = time::precise_time_ns();
+    let primes_vec = primes::primes(13000);
+    let runtime = (time::precise_time_ns() - t0) / 1000;
+    println!("{:?}", primes_vec);
+    println!("Runtime: {:?} microseconds", runtime);   //  runtime is about
 }
