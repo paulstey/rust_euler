407015f5e3e134b6d705988151824ab0451b48e4
paulstey <paulstey@gmail.com>
2020-01-05 19:56:43 -0500

    rename palindrome lib

--- prob4/src/is_palindrome.rs
+++
@@ -1,7 +0,0 @@
-// is_palidrome.rs
-pub fn is_palidrome(num: i64) -> bool {
-    let num_str = num.to_string();
-    let rev_num_str = num_str.chars().rev().collect::<String>();
-
-    num_str == rev_num_str
-}
