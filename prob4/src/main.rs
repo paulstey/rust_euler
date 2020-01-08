c50824decb5cd23c4568561a665c1f58e3fd8a11
paulstey <paulstey@gmail.com>
2020-01-07 20:51:35 -0500

    use new modulo palindrome check

--- prob4/src/main.rs
+++ prob4/src/main.rs
@@ -1,15 +1,28 @@
 extern crate time;
 
-
 use prob4::palindr;
 
+fn largest_palindrome_prod(max_n: u64) -> u64 {
+    let mut soln: u64 = 0;
+    for i in 1..max_n {
+        for j in 1..max_n {
+            soln = if palindr::is_palindrome(i*j) {
+                i*j
+            }
+            else {
+                soln
+            }
+        } 
+    }
+    soln
+}
 
 fn main() {
     let t0: u64 = time::precise_time_ns();
-
-
+    let soln = largest_palindrome_prod(999 as u64);
 
     let time_ms = (time::precise_time_ns() - t0) / 1000;
 
+    println!("{:?}", soln);
     println!("Runtime: {:?} microseconds", time_ms);   //  runtime is about
 }
--- prob4/src/palindr.rs
+++ prob4/src/palindr.rs
@@ -1,26 +1,28 @@
 // palidnr.rs
 
 
-pub fn is_palidrome(num: i64) -> bool {
+pub fn is_palindrome_str(num: u64) -> bool {
     let num_str = num.to_string();
     let rev_num_str = num_str.chars().rev().collect::<String>();
 
     num_str == rev_num_str
 }
 
-pub fn reverse_int<T>(x: T) -> T {
-    let result = 0;
-    let x_remaining = abs(x);
+
+// NOTE: Using this is _vastly_ more efficient (and faster) than the 
+// above function that reliese on string conversion
+pub fn is_palindrome(num: u64) -> bool {
+    reverse_int(num) == num
+}
+
+
+pub fn reverse_int(x: u64) -> u64{
+    let mut result = 0;
+    let mut x_remaining = x;
 
     while x_remaining > 0 {
-        let result = result * 10 + x_remaining % 10;
+        result = result * 10 + x_remaining % 10;
         x_remaining /= 10;
     }
-    let soln = if x < 0 {
-        -result;
-    } 
-    else {
-        result;
-    }
-    soln
+    result
 }
