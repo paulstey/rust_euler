3161bebde82a11fc0777375b140235c694a1217c
Paul Stey <paulstey@gmail.com>
2021-12-02 23:23:58 -0500

    feat: solve 8 and start 9

--- prob8/src/main.rs
+++ prob8/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs;
 use std::io;
+use time::Instant;
+
+
 
 fn read_txt_file(filename: &str) -> Result<String, io::Error> {
     let dat = fs::read_to_string(filename)?;
@@ -7,20 +10,32 @@ fn read_txt_file(filename: &str) -> Result<String, io::Error> {
 }
 
 fn main() -> Result<(), io::Error> {
-
+    let t0 = Instant::now();
     let dat = read_txt_file("src/data.txt")?;
 
-
-    let num_vec: Vec<u32> = dat
+    let num_vec: Vec<u128> = dat
         .replace("\n", "")
         .chars()
         .map(|c| {
-            c.to_string().parse::<u32>().unwrap()
+            c.to_string().parse::<u128>().unwrap()
         })
         .collect();
 
-    println!("{:?}", num_vec);
+    let mut running_prod = Vec::new();
+    let mut prod: u128;
+    
+    let last_idx = num_vec.len() - 12;
 
+    for i in 0..last_idx {
+        prod = num_vec[i] * num_vec[i+1] * num_vec[i+2] * num_vec[i+3] * 
+            num_vec[i+4] * num_vec[i+5] * num_vec[i+6] * num_vec[i+7] *
+            num_vec[i+8] * num_vec[i+9] * num_vec[i+10] * num_vec[i+11] * num_vec[i+12]; 
+
+        running_prod.push(prod);
+    }
+
+    println!("{:?}", running_prod.iter().max());
+    println!("{:?}", t0.elapsed());
 
     Ok(())
 }
