151ab84e80782f26621ef5ad304482c14b763142
Paul Stey <paulstey@gmail.com>
2021-12-02 23:44:38 -0500

    feat: solve prob 9

--- prob9/src/main.rs
+++ prob9/src/main.rs
@@ -1,3 +1,25 @@
+use time::Instant;
+
+
 fn main() {
-    println!("Hello, world!");
+    let t0 = Instant::now();
+
+    'outer: for a in 1..999 {
+        for b in 1..999 {
+            for c in 1..999 {
+                if a + b + c == 1000 {
+                    if i32::pow(a, 2) + i32::pow(b, 2) == i32::pow(c, 2) {
+                         let res = a*b*c;
+
+                         println!("{:?}", res);
+
+                         break 'outer;
+                    }
+                }
+            }
+        }
+    }
+    println!("{:?}", t0.elapsed());
+
+    ()
 }
