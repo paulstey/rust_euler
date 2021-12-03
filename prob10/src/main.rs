158f87cc79e204ef693516072f72728c576728f3
Paul Stey <paulstey@gmail.com>
2021-12-03 00:10:01 -0500

    wip: start problem 10

---
+++ prob10/src/main.rs
@@ -0,0 +1,12 @@
+use primes::{Sieve, PrimeSet};
+use time::Instant;
+
+fn main() {
+    let t0 = Instant::now();
+    let mut pset = Sieve::new();
+
+    let res: u64 = pset.iter().take(300_000).sum();
+
+    println!("{:?}", res);
+    println!("{:?}", t0.elapsed());
+}
