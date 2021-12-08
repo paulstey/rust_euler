87cbbcde9ac23be5b63b8c7386386a871198e506
Paul Stey <paulstey@gmail.com>
2021-12-07 21:44:10 -0500

    feat: solve prob10

--- prob10/src/main.rs
+++ prob10/src/main.rs
@@ -5,8 +5,16 @@ fn main() {
     let t0 = Instant::now();
     let mut pset = Sieve::new();
 
-    let res: u64 = pset.iter().take(300_000).sum();
+    let max_n = 2_000_000; 
+    let prime_vec: Vec<u64> = pset.iter().take(max_n).collect();
 
+    let mut res = 0_u64;
+
+    for prime in prime_vec {
+        if prime < max_n.try_into().unwrap() {
+            res += prime;
+        }
+    }
     println!("{:?}", res);
     println!("{:?}", t0.elapsed());
 }
