cd1c530da7d224756afb8566397b567a27413501
paulstey <paulstey@gmail.com>
2021-12-19 15:21:26 -0500

    feat: solved problem 20

---
+++ prob20/src/main.rs
@@ -0,0 +1,28 @@
+use time::Instant;
+use num_bigint::BigInt;
+
+fn factorial(n: i32) -> BigInt {
+
+    let mut res: BigInt = BigInt::from(n);
+
+    for m in 2..n {
+        res *= BigInt::from(m);
+    }
+    res
+}
+
+fn main() {
+    let t0 = Instant::now();
+
+    let num: BigInt = factorial(100);
+
+    let soln: u32 = num
+        .to_string()
+        .chars()
+        .map(|x| x.to_digit(10_u32).unwrap())
+        .sum();
+
+    println!("{:?}", soln);
+
+    println!("{:?}", t0.elapsed());
+}
