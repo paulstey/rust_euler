38ed0cffd010073954e5bd12a025d5d2528a4abf
Paul Stey <paulstey@gmail.com>
2022-02-01 23:11:06 -0500

    feat: solved prob12 with slow brute force

--- prob12/src/main.rs
+++ prob12/src/main.rs
@@ -1,12 +1,24 @@
 ///! src/main.rs
 use time::Instant;
-use primes;
+
+const MAX_NUM: i32 = 100_000_000;
+
+fn get_multiples(n: i32) -> Vec<i32> {
+    let mut res_vec: Vec<i32> = Vec::new();
+
+    for i in 1..=n {
+        if n % i == 0 {
+            res_vec.push(i);
+        }
+    }
+    res_vec
+}
 
 fn main() {
     let t0 = Instant::now();
 
-    let a_vec: Vec<u64> = (1..=100).collect();
-    let cumsum: Vec<u64> = a_vec
+    let a_vec: Vec<i32> = (1..=MAX_NUM).collect();
+    let cumsum: Vec<i32> = a_vec
         .iter()
         .scan(0, |acc, &x| {
             *acc += x;
@@ -14,13 +26,15 @@ fn main() {
         })
         .collect();
 
-    let t1 = Instant::now();
-
     for x in cumsum {
-        println!("{:?}", x);
-        println!("{:?}", primes::factors_uniq(x)); 
+        if get_multiples(x).len() > 500 {
+            println!("{:?}", x); 
+            break 
+        }
     }
+    
 
-
+    let t1 = Instant::now();
+    
     println!("{:?}", t1 - t0);
 }
