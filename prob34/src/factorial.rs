49034d8c853e554c17a1cfeed6a02704458b4bd7
Paul Stey <paulstey@gmail.com>
2022-07-01 20:36:19 -0400

    wip: working prob34 about factorial sums

--- prob34/src/factorial.rs
+++ prob34/src/factorial.rs
@@ -1,14 +1,17 @@
 //! /src/iterator.rs
-//!
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Factorial {
     pair: (i64, i64),
+    end: i64,
 }
 
 impl Factorial {
     pub fn new(start: i64) -> Self {
-        Factorial { pair: (start, 1) }
+        Factorial {
+            pair: (1, 1),
+            end: start,
+        }
     }
 }
 
@@ -16,10 +19,9 @@ impl Iterator for Factorial {
     type Item = (i64, i64);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.pair.0 > 1 {
+        if self.pair.0 < self.end {
+            self.pair.0 += 1;
             self.pair.1 *= self.pair.0;
-            self.pair.0 -= 1;
-
             Some(self.pair)
         } else {
             None
--- prob34/src/main.rs
+++ prob34/src/main.rs
@@ -3,14 +3,11 @@ use prob34::factorial::Factorial;
 fn main() {
     let mut fact_iter = Factorial::new(13 as i64);
 
-    println!("{:?}", fact_iter.next()); 
-
-    loop { 
+    loop {
         if let Some(fact_iter) = fact_iter.next() {
             println!("{:?}", fact_iter);
-        }
-        else {
-            break
+        } else {
+            break;
         }
     }
 }
