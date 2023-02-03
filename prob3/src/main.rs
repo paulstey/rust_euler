7daf858a33b2a1ad9db4fd86dfc1f9880bac19eb
Paul Stey <paulstey@gmail.com>
2023-02-03 14:02:15 -0500

    fix: error in prob3 time library

--- prob3/src/main.rs
+++ prob3/src/main.rs
@@ -1,13 +1,13 @@
 
-use time;
+use time::Instant;
 use prob3::primes;
 
 
 
 fn main() {
-    let t0: u64 = time::precise_time_ns();
+    let t0 = Instant::now();
     let primes_vec = primes::primes(13000);
-    let runtime = (time::precise_time_ns() - t0) / 1000;
+    let runtime = Instant::now() - t0;
     println!("{:?}", primes_vec);
-    println!("Runtime: {:?} microseconds", runtime);   //  runtime is about
+    println!("{:?}", runtime);   //  runtime is about
 }
