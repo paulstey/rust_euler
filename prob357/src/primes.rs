9eeaf0da877858eccb3b73b454184d57fd7a3334
Paul Stey <paulstey@gmail.com>
2024-03-03 11:34:44 -0500

    feat: solve problem 357 with parallel prime generation and btreeset

---
+++ prob357/src/primes.rs
@@ -0,0 +1,30 @@
+// primes.rs
+
+use rayon::prelude::*;
+
+pub fn is_prime(n: u64) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    if n <= 3 {
+        return true;
+    }
+    if n % 2 == 0 || n % 3 == 0 {
+        return false;
+    }
+    let mut i = 5;
+    while i * i <= n {
+        if n % i == 0 || n % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
+    }
+    true
+}
+
+pub fn primes(limit: u64) -> Vec<u64> {
+    (2..limit)
+        .into_par_iter()
+        .filter(|&x| is_prime(x))
+        .collect()
+}
