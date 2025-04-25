740efa335eff8ccd3f224ce877459ee1676f0e9f
Paul Stey <paulstey@gmail.com>
2025-04-24 21:07:55 -0400

    feat: solve prob 70

--- prob70/submit.sh
+++ prob70/submit.sh
@@ -4,12 +4,12 @@
 # In order to submit this job to Slurm, run the command below from the terminal.
 # `sbatch submit.sh`
 
-#SBATCH --time=4:00:00
+#SBATCH --time=96:00:00
 #SBATCH --partition=batch
 #SBATCH --nodes=1
 #SBATCH --constraint=192core
-#SBATCH --cpus-per-task=102
-#SBATCH --mem=64G
+#SBATCH --cpus-per-task=192
+#SBATCH --mem=94G
 #SBATCH --job-name prob70 
 #SBATCH --output prob70-%j.out
 #SBATCH --error prob70-%j.err
